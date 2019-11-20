048e02303a35905d05d7d1e89b03890413dbca77
liting zhao <emily51905@163.com>
2019-11-20 11:22:43 +0800

    not finished yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+articles.destroy_all
+
+10.times do
+  Article.create(title: Faker::Books::Dune, content: Faker::Books::Dune.quote)
+end
